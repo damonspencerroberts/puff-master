97dc77994b13dd0be273582a312e0ae2e6bd6bf1
Damon Roberts <damon@weekendr.eu>
2021-08-17 21:13:18 +0200

    created puff master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [count, setCount] = useState('');
+
+  useEffect(() => {
+    fetch('https://puff-master-default-rtdb.europe-west1.firebasedatabase.app/amount.json')
+      .then((res) => res.json())
+      .then((res) => setCount(parseInt(res, 10)));
+  }, []);
+
+  const handleChangeCount = async (amt) => {
+    setCount(parseInt(amt, 10));
+    await axios
+      .put('https://puff-master-default-rtdb.europe-west1.firebasedatabase.app/amount.json', amt)
+      .then((res) => console.log(res)).catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header_container">
+        <div className="header">Puff Master</div>
+      </div>
+      <div className="main">
+        <div className="count">Count: {count}</div>
+        <div role="button" className="button" onClick={() => handleChangeCount(count + 1)}>
+          Add
+        </div>
+        <div role="button" className="restart" onClick={() => handleChangeCount('0')}>
+          Restart
+        </div>
+      </div>
     </div>
   );
 }
